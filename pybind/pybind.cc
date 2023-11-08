fb13c583563b719719a2b9acea1d2a525e3c72ba
vam-google <vam@google.com>
2023-11-08 12:28:49 -0800

    chore: reformat includes ordering to match internal build

--- pybind/pybind.cc
+++ pybind/pybind.cc
@@ -3,7 +3,7 @@
 #include "pybind11/pybind11.h"
 
 PYBIND11_MODULE(pybind_cc_binary, m) {
-    m.doc() = "pybind11 example plugin"; // optional module docstring
+    m.doc() = "pybind11 example plugin";
     m.def("first_func", &first_func, "The first function");
     m.def("second_func", &second_func, "The second function");
 }
