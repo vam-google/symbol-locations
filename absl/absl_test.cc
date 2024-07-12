c2364f5775407f3a5858dd406707502e65ca3be4
vam-google <vam-google@google.com>
2024-07-12 15:58:17 -0700

    feat: fix absl tests for windows

--- absl/absl_test.cc
+++ absl/absl_test.cc
@@ -7,10 +7,13 @@ TEST(AbslTest, AbslTest) {
   std::cout << "AbslTest START" << std::endl;
   EXPECT_EQ(17, flat_hash_map_check(17));
 
+  std::cout << "AbslTest 1" << std::endl;
   absl::flat_hash_map<std::string, int> map;
+  std::cout << "AbslTest 2" << std::endl;
   map["one"] = 18;
-
+  std::cout << "AbslTest 3" << std::endl;
   EXPECT_EQ(18, map["one"]);
+  std::cout << "AbslTest 4" << std::endl;
 
   std::cout << "AbslTest END" << std::endl;
 }
