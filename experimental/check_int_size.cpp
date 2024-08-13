c4e3f9bbafdda06dd796e849c9944a6e11ba0454
vam-google <vam@google.com>
2024-08-13 13:26:35 -0700

    chore: add experimental casting check for mac tests

--- experimental/check_int_size.cpp
+++ experimental/check_int_size.cpp
@@ -18,16 +18,23 @@ int main() {
   std::cout << "sizeof(long long): " << sizeof(long long) << std::endl;
   std::cout << "sizeof(int64_t): " << sizeof(int64_t) << std::endl;
   std::cout << "sizeof(intptr_t): " << sizeof(intptr_t) << std::endl;
+  std::cout << "std::is_same_v<int64_t, intptr_t>: " << std::is_same_v<int64_t, intptr_t> << std::endl;
+  std::cout << "std::is_same_v<int64_t, long>: " << std::is_same_v<int64_t, long> << std::endl;
+  std::cout << "std::is_same_v<int64_t, long long>: " << std::is_same_v<int64_t, long long> << std::endl;
+  std::cout << "std::is_same_v<intptr_t, long>: " << std::is_same_v<intptr_t, long> << std::endl;
+  std::cout << "std::is_same_v<intptr_t, long>: " << std::is_same_v<intptr_t, long long> << std::endl;
+
+
 
 //  std::vector<intptr_t> factorIndices;
 //  factorIndices.push_back(123L);
 //  factorIndices.push_back(456L);
 //  foo(factorIndices.data());
 
-  std::vector<int64_t> factorIndices1;
-  factorIndices1.push_back(1230L);
-  factorIndices1.push_back(4560L);
-  foo1(static_cast<const intptr_t*>(factorIndices1.data()));
+//  std::vector<int64_t> factorIndices1;
+//  factorIndices1.push_back(1230L);
+//  factorIndices1.push_back(4560L);
+//  foo1(static_cast<const intptr_t*>(factorIndices1.data()));
 
 
   return 0;
