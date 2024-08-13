7217e9860fa6c6b72312a25af9d16c1b0865bccc
vam-google <vam@google.com>
2024-08-13 11:58:50 -0700

    chore: add experimental casting check for mac tests

--- experimental/check_int_size.cpp
+++ experimental/check_int_size.cpp
@@ -7,6 +7,11 @@ void foo(const intptr_t* factorSizes) {
   std::cout << factorSizes[1] << std::endl;
 }
 
+void foo1(const intptr_t* factorSizes) {
+  std::cout << factorSizes[0] << std::endl;
+  std::cout << factorSizes[1] << std::endl;
+}
+
 int main() {
   std::cout << "sizeof(int): " << sizeof(int) << std::endl;
   std::cout << "sizeof(long): " << sizeof(long) << std::endl;
@@ -15,11 +20,15 @@ int main() {
   std::cout << "sizeof(intptr_t): " << sizeof(intptr_t) << std::endl;
 
   std::vector<intptr_t> factorIndices;
-
   factorIndices.push_back(123L);
   factorIndices.push_back(456L);
-
   foo(factorIndices.data());
+
+  std::vector<int64_t> factorIndices1;
+  factorIndices1.push_back(1230L);
+  factorIndices1.push_back(4560L);
+  foo1(factorIndices1.data());
+
 
   return 0;
 }
